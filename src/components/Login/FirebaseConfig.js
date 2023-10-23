11dba31eb471003d17bf5fffefcd8cba58eaa2dc
Izet Delibasic <izetdelibasic55@gmail.com>
2023-10-23 22:36:11 +0200

    Setting up firebase

---
+++ src/components/Login/FirebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOXIF-wmb5B9ZVCO-Bvqis6rYwlFQhYXE",
+  authDomain: "winzone-84a23.firebaseapp.com",
+  projectId: "winzone-84a23",
+  storageBucket: "winzone-84a23.appspot.com",
+  messagingSenderId: "816871522872",
+  appId: "1:816871522872:web:7f0295f060ed1af60edccd"
+};
+
+const app = initializeApp(firebaseConfig);
