b4c4f83e9bf81479d9fae57695efeaae784fa0c3
Izet Delibasic <izetdelibasic55@gmail.com>
2023-10-23 22:14:04 +0200

    Importing all files from competition

--- src/components/Achievements/Achievements.js
+++ src/components/Achievements/Achievements.js
@@ -1,13 +1,18 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // Dodali smo Link
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './Achievements.css';
 
 export const Achievements = () => {
   const [achievements, setAchievements] = useState([
-    { id: 1, name: 'Achievement 1', completed: false },
-    { id: 2, name: 'Achievement 2', completed: true },
-    { id: 3, name: 'Achievement 3', completed: false },
+    { id: 1, name: 'First login', completed: false },
+    { id: 2, name: 'Login two times in one hour', completed: false },
+    { id: 3, name: 'First deposit', completed: false },
   ]);
+
+  const [collectedAchievements, setCollectedAchievements] = useState([]);
+
+  // Premestite izračunavanje levelProgress ovde nakon inicijalizacije collectedAchievements i achievements.
+  const levelProgress = (collectedAchievements.length / achievements.length) * 100;
 
   const collectAchievement = (id) => {
     setAchievements((prevAchievements) =>
@@ -15,7 +20,13 @@ export const Achievements = () => {
         achievement.id === id ? { ...achievement, completed: true } : achievement
       )
     );
+
+    setCollectedAchievements((prevCollectedAchievements) => [
+      ...prevCollectedAchievements,
+      id,
+    ]);
   };
+
 
   return (
     <div className="container-achievements">
@@ -25,11 +36,16 @@ export const Achievements = () => {
           {achievements.map((achievement) => (
             <li key={achievement.id}>
               <div className="achievement">
-                <div className="ach1" alt={`Logo za ${achievement.name}`} />
+                <div
+                  className={`ach${achievement.id}`}
+                  alt={`Logo za ${achievement.name}`}
+                />
                 <div className="achievement-details">
                   <h3>{achievement.name}</h3>
                   <div className="progress-bar">
-                    <div className={`progress ${achievement.completed ? 'completed' : ''}`}>
+                    <div
+                      className={`progress ${achievement.completed ? 'completed' : ''}`}
+                    >
                       {achievement.completed ? 'Completed' : '0/1'}
                     </div>
                   </div>
@@ -45,19 +61,41 @@ export const Achievements = () => {
             </li>
           ))}
         </ul>
-      </div>
+        <div class="skill">
+            <div class="jenengskill">Achievements Progress: </div> 
+            <div class="skill-bar">
+                <div class="htmlcss" style={{ width: `${levelProgress}%` }}>
+                  <div class="bunder1"><span>{Math.round(levelProgress)}%</span></div>
+                </div>
+              </div>
+            </div>
+        </div>
+
+
       <div className="nickname">
         <div className="container">
           <div className="box">
+            <div className="name_job">Izet Delibasic</div>
             <div className="image">
               
             </div>
-            <div className="name_job">Izet Delibasic</div>
-            <div className="rating">
-              
+            <div className="rating"></div>
+            <div className="my-achievements">
+              <h3>My Achievements</h3>
+              <ul>
+                {collectedAchievements.map((collectedId) => (
+                  <li key={collectedId}>
+                    <div
+                      className={`ach${collectedId} profile-image`}
+                      alt={`Logo za Achievement ${collectedId}`}
+                    />
+                  </li>
+                ))}
+              </ul>
             </div>
             <div className="btns">
-              <Link to="/"> 
+              <hr />
+              <Link to="/">
                 <button>Sign Out</button>
               </Link>
             </div>
